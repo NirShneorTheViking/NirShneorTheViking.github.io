4920d2d158fc43d85b4193db7b6b355e7dee287a
NirShneorTheViking <87433003+NirShneorTheViking@users.noreply.github.com>
2024-09-12 14:57:18 +0300

    Update and rename nav.html to nav.js

---
+++ nav.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">\
+        <div class="container">\
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+              <ul class="nav navbar-nav navbar-right">\
+                <li>\
+                  <a href="index.html">Home</a>\
+                </li>\
+                <li>\
+                  <a href="about.html">About Me</a>\
+                </li>\
+                <li>\
+                  <a href="norway.html">Crossing The Norwegian Mountains</a>\
+                </li>\
+              </ul>\
+            </div>\
+        <!-- /.navbar-collapse -->\
+        </div>\    
+    </nav>\
+\
+');
